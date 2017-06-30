702f5c0ae7859d6bbdbd70d980ff301d883525c1
AP Pranav <pranavap96@gmail.com>
2017-07-01 05:27:40 +0530

    cnn with val_acc 93.4

--- Convolutional_Neural_Networks/cnn.py
+++ Convolutional_Neural_Networks/cnn.py
@@ -15,7 +15,7 @@ classifier = Sequential()
 # its dimensions within (rows, cols)
 # input_shape - to transform all images to standard format (width, height, channel)
 # activation function used for more non linearity
-classifier.add(Conv2D(64, (3, 3), input_shape = (256, 256, 3), activation = 'relu'))
+classifier.add(Conv2D(32, (3, 3), input_shape = (256, 256, 3), activation = 'relu'))
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
 # Adding a second convolutional layer
@@ -25,16 +25,17 @@ classifier.add(MaxPooling2D(pool_size = (2, 2)))
 classifier.add(Conv2D(64, (3 ,3), activation = 'relu'))
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
+classifier.add(Conv2D(128, (3 ,3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
 # Step 3 - Flattening
 classifier.add(Flatten())
 
 # Step 4 - Full connection
+classifier.add(Dropout(0.6))
 classifier.add(Dense(units = 128, activation = 'relu'))
-classifier.add(Dropout(0.1))
 
-classifier.add(Dense(units = 128, activation = 'relu'))
-classifier.add(Dropout(0.1))
-
+classifier.add(Dropout(0.3))
 classifier.add(Dense(units = 1, activation = 'sigmoid'))
 
 print('\n compiling the CNN');
@@ -52,27 +53,27 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
 
 test_datagen = ImageDataGenerator(rescale = 1./255)
 
-training_set = train_datagen.flow_from_directory('dataset/training_set',
+training_set = train_datagen.flow_from_directory('/input/dataset/training_set',
                                                  target_size = (256, 256),
                                                  batch_size = 32,
                                                  class_mode = 'binary')
 
-test_set = test_datagen.flow_from_directory('dataset/test_set',
+test_set = test_datagen.flow_from_directory('/input/dataset/test_set',
                                             target_size = (256, 256),
                                             batch_size = 32,
                                             class_mode = 'binary')
 
 classifier.fit_generator(training_set,
-                         steps_per_epoch = 8000/32,
-                         epochs = 50,
+                         steps_per_epoch = 8000,
+                         epochs = 100,
                          validation_data = test_set,
-                         validation_steps = 2000/32)
+                         validation_steps = 2000)
 
 # Make single predictions
 import numpy as np
 from keras.preprocessing import image
-print('\n Make single predictions: dataset/test_set/cats/cat.4001.jpg')
-test_image = image.load_img('dataset/test_set/cats/cat.4001.jpg', target_size = (256, 256))
+print('\n Make single predictions: /input/dataset/test_set/cats/cat.4001.jpg')
+test_image = image.load_img('/input/dataset/test_set/cats/cat.4001.jpg', target_size = (256, 256))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
 result = classifier.predict(test_image)
@@ -82,8 +83,6 @@ if result[0][0] == 1:
 else:
   print('It is a cat')
 
-# loss: 0.1028 - acc: 0.9623 - val_loss: 0.4677 - val_acc: 0.8550
 
-
-
+# loss: 0.0634 - acc: 0.9778 - val_loss: 0.2141 - val_acc: 0.9346
 
